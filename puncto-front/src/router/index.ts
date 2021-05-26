734eb3d2d9e8168d9d7d335e07198e6b65ddc278
Lívia Almeida <leave.ah@gmail.com>
2021-05-25 23:10:48 -0300

    Added initial view files

--- puncto-front/src/router/index.ts
+++ puncto-front/src/router/index.ts
@@ -1,23 +1,38 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/login',
+    name: 'Login',
+    component: () => import('@/views/Login.vue'),
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/',
+    name: 'Gerenciamento de ponto',
+    component: () => import('@/views/Home.vue'),
+		meta: {
+			auth: true,
+		},
+  },
+  {
+    path: '/cadastro',
+    name: 'Cadastro',
+    component: () => import('@/views/Registration.vue'),
+		meta: {
+			auth: true,
+		},
+  },
+  {
+    path: '/clientes',
+    name: 'Clientes',
+    component: () => import('@/views/Clients.vue'),
+		meta: {
+			auth: true,
+		},
+  },
 ]
 
 const router = new VueRouter({
