2926126c9cd7389379d26bcf957d8a56f9629b62
Lívia Almeida <leave.ah@gmail.com>
2021-05-25 23:16:45 -0300

    Removed default app styles

--- puncto-front/src/App.vue
+++ puncto-front/src/App.vue
@@ -36,11 +36,10 @@ export default Vue.extend({
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Roboto', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   height: 100vh;
   display: flex;
 
