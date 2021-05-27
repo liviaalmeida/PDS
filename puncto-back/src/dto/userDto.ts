23154777f2ad172b8fd145182a60e3671d4d52a7
Gabriel Chaves <gabriel.chaves@thoughtworks.com>
2021-05-27 18:24:32 -0300

    Created unit tests for UserService

--- puncto-back/src/dto/userDto.ts
+++ puncto-back/src/dto/userDto.ts
@@ -2,4 +2,10 @@ export class UserDto {
     firstName: string;
     lastName: string;
     age: number;
+
+    public constructor(firstName: string, lastName: string, age: number) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
 }
