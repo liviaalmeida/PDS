84de3ef48787c689a5e11053e303653e62d43d01
Lívia Almeida <leave.ah@gmail.com>
2021-05-30 14:45:34 -0300

    Added years logic

--- puncto-front/src/components/calendar/PtYears.vue
+++ puncto-front/src/components/calendar/PtYears.vue
@@ -5,11 +5,21 @@
         <th>
           <button @click="offset--">◄</button>
         </th>
+        <th></th>
         <th>
           <button @click="offset++">►</button>
         </th>
       </tr>
     </thead>
+    <tbody>
+      <tr v-for="(row, index) in years" :key="index">
+        <td v-for="year in row" :key="year">
+          <button @click="$emit('year', year)">
+            {{ year }}
+          </button>
+        </td>
+      </tr>
+    </tbody>
   </table>
 </template>
 
@@ -28,8 +38,22 @@ export default Vue.extend({
     }
   },
   computed: {
-    years() {
-      return []
+    start(): number {
+      return this.year - (this.year % 16) + (this.offset * 16)
+    },
+    years(): number[][] {
+      return Array.from(
+        { length: 4 },
+        (_, index) => this.sequence(this.start + index * 3, 3)
+      )
+    },
+  },
+  methods: {
+    sequence(start: number, length: number) {
+      return Array.from(
+        { length },
+        (_, index) => start + index,
+      )
     },
   },
 })
