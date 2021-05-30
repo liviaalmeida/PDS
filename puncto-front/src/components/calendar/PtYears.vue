6f8c721ef48fe00dac38a6ff8125c3858fc1d2e8
Lívia Almeida <leave.ah@gmail.com>
2021-05-29 21:41:06 -0300

    Started year pick component

---
+++ puncto-front/src/components/calendar/PtYears.vue
@@ -0,0 +1,36 @@
+<template>
+  <table class="calendar-pick">
+    <thead>
+      <tr>
+        <th>
+          <button @click="offset--">◄</button>
+        </th>
+        <th>
+          <button @click="offset++">►</button>
+        </th>
+      </tr>
+    </thead>
+  </table>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    year: {
+      required: true,
+      type: Number,
+    },
+  },
+  data() {
+    return {
+      offset: 0,
+    }
+  },
+  computed: {
+    years() {
+      return []
+    },
+  },
+})
+</script>
