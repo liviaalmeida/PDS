b532db248eb03582eff888d251fc48b1276d124e
Philipe Pinheiro Atela <philipeatela@gmail.com>
2021-06-15 19:03:13 -0300

    Feat/auth middleware (#12)
    * feat: validation middleware & login endpoint validation
    
    * chore: swap libraries to fix build
    
    * fix: jest regex to match js files only
    
    * feat: auth middleware
    
    * fix: jest config
    
    Co-authored-by: philipeatela <philipe@videopeel.com>

---
+++ puncto-back/src/middlewares/authentication.ts
@@ -0,0 +1,25 @@
+import { Request } from 'express';
+
+import { AuthInfo, AuthService } from '../service/authService'
+import { container } from '../inversify.config';
+
+const authService = container.get(AuthService);
+
+// esse middleware decodifica o JWT recebido no header da requisicao e injeta
+// o email do usuario no objeto Request
+export const authMiddleware = (req: Request, res, next) => {
+  if (!req.headers.authorization) {
+    res.status(401).json('Missing authentication token');
+  }
+
+  // From `Bearer <token>` to `<token>`
+  const bearerToken = req.headers.authorization?.split(' ')[1] || '';
+
+  try {
+    const authInfo: AuthInfo = authService.verifyToken(bearerToken);
+    req.userEmail = authInfo.userEmail;
+    next()
+  } catch(error) {
+    res.status(401).json('Invalid authentication token');
+  }
+}
