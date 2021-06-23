887c2cbfad2976a0d0b0122c331907aacdd3bbc4
brunacmelo <brunacmelo14@gmail.com>
2021-06-23 13:33:33 -0300

    Removi comentarios desnecessarios

--- puncto-front/src/App.vue
+++ puncto-front/src/App.vue
@@ -60,12 +60,6 @@ export default Vue.extend({
   .view {
     flex: 1;
     overflow: auto;
-    //padding: 20px;
-    /*position: absolute;
-    width: 100%;
-    height: 100%;
-    background: url("background.png");
-=======*/
     padding: 20px;
     &-header {
       display: flex;
