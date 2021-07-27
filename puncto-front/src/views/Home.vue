a62f6c66673f3bad1f71292839ee1f83972d0cf4
Lívia Almeida <leave.ah@gmail.com>
2021-07-27 01:09:35 -0300

    Added time-group component

--- puncto-front/src/views/Home.vue
+++ puncto-front/src/views/Home.vue
@@ -2,48 +2,47 @@
   <div class="home d-flex flex justify-content-center">
     <div class="flex-column justify-content-center">
       <TimeCurrent class="home-header" />
-      <PtCalendar v-model="daySelected"
-      :fullfilled="[]" :pending="[]" />
+      <div class="home-calendar">
+        <PtCalendar v-model="daySelected"
+        :fullfilled="[]" :pending="[]" />
+      </div>
     </div>
     <div class="flex-column justify-content-center">
       <div class="home-header">
-        <PtButton class="home-button">Registrar Ponto</PtButton>
+        <PtButton class="home-button" :disabled="editing">
+          Registrar Ponto
+        </PtButton>
       </div>
-      <div class="home-punchs d-flex flex-column">
-        <div class="d-flex flex-row justify-content-between">
-          <div class="home-daySelected">{{ daySelectedText }} </div> 
-          <div class="d-flex flex-row justify-content-between align-items-center">
-            <div class="home-daySelected">{{ hourSelected }} </div>
-            <div class="home-tag"></div>
-          </div>
-        </div>
-        <TimeRegistry v-for="punch in punchs"
-        :key="punch.id" :punch="punch" />
-        <PtButton round @click="onAdd">+</PtButton>
-      </div>
+      <TimeGroup :punches="punches"
+      :day="daySelected"
+      :editing="editing"
+      class="home-punches"
+      @add="onAdd"
+      @delete="onDelete"
+      @edit="editing = $event"
+      @save="onSave" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import moment from 'moment'
 import TimeCurrent from '../components/time-register/TimeCurrent.vue'
-import TimeRegistry from '../components/time-register/TimeRegistry.vue'
-
-moment.locale('pt-br')
+import TimeGroup from '../components/time-register/TimeGroup.vue'
+import { Punch } from '../domain/Punch'
 
 export default Vue.extend({
   name: 'Home',
   components: {
     TimeCurrent,
-    TimeRegistry,
+    TimeGroup,
   },
   data() {
     return {
       daySelected: new Date(),
-      hourSelected: '8:45',
-      punchs: [
+      duration: '',
+      editing: false,
+      punches: [
         {
           id: '05102012',
           start: '06:40',
@@ -54,27 +53,26 @@ export default Vue.extend({
         {
           id: '05102013',
           start: '12:30',
-          end: '15:25',
+          end: '14:30',
           client: 'A Fantástica Fábrica de Chocolate',
           activity: 'Teste da receita secreta'
         }
-      ],
+      ] as Punch[],
     }
   },
-  computed: {
-    daySelectedText(): string {
-      return moment(this.daySelected).format('LL').toLowerCase()
-    },
-  },
   methods: {
-    editPonto() {
-      alert('Editar ponto')
+    onAdd(punch: Punch) {
+      window.alert(`add ${punch.start} ${punch.end}`)
+      this.punches = [
+        ...this.punches,
+        punch,
+      ]
     },
-    excluirPonto() {
-      alert('Excluir ponto')
+    onDelete(id: string) {
+      window.alert(`delete ${id}`)
     },
-    onAdd() {
-      alert('Adicionar novo Ponto')
+    onSave(punch: Punch) {
+      window.alert(`save ${punch.id}`)
     },
   },
 })
@@ -93,28 +91,10 @@ export default Vue.extend({
     width: 281px;
   }
 
-  &-punchs {
+  &-calendar, &-punches {
     background: rgba($pt-fog, .5);
     border-radius: 5px;
-    gap: 20px;
     padding: 15px;
-    width: 580px;  
-  }
-
-  &-daySelected {
-    font-family: Ubuntu;
-    font-weight: bold;
-    font-size: 20px;
-    align-items: center;
-    color: $pt-midnight;
-  }
-
-  &-tag {
-    background-color: $pt-sapphire;
-    border-radius: 50%;
-    height: 20px;
-    width: 20px;
-    margin-left: 10px;
   }
 }
 </style>
