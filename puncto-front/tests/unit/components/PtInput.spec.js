cec2dad5ba528ff33f873368871fd73b40ffbf36
Lívia Almeida <leave.ah@gmail.com>
2021-07-28 15:43:03 -0300

    Updated unit tests

--- puncto-front/tests/unit/components/PtInput.spec.js
+++ puncto-front/tests/unit/components/PtInput.spec.js
@@ -1,23 +1,57 @@
-import { shallowMount } from '@vue/test-utils'
-import PtIcon from '@/components/PtIcon.vue'
-import PtInput from '@/components/input/PtInput.vue'
+import { mount, createLocalVue } from '@vue/test-utils'
+import PtIcon from '@/common/PtIcon.vue'
+import PtInput from '@/common/input/PtInput.vue'
+import VueMask from 'v-mask'
+import Vue from 'vue'
+
+const localVue = createLocalVue()
+localVue.use(VueMask)
 
 describe('PtInput', () => {
   const label = 'Test Input'
-  let ptInput
+  const type = 'number'
+  let ptInput, input
 
   beforeEach(() => {
-    ptInput = shallowMount(PtInput, {
+    ptInput = mount(PtInput, {
+      localVue,
       propsData: {
         label,
+        type,
+        value: 'kkkkkk'
+      },
+      model: {
+        prop: 'value',
       },
       components: {
         PtIcon,
       },
     })
+    input = ptInput.find('input')
   })
 
   it('renders a input', () => {
-    expect(ptInput.find('input')).toBeDefined()
+    expect(input.exists()).toBeDefined()
+  })
+
+  it('renders the input with type', () => {
+    const inputType = input.attributes('type')
+    expect(inputType).toBe(type)
+  })
+
+  it('renders the input label with text', () => {
+    const labelWrapper = ptInput.find('label')
+    expect(labelWrapper.text()).toBe(label)
+  })
+
+  it('emits an input event on input input', () => {
+    input.trigger('input')
+    expect(ptInput.emitted().input?.length).toBe(1)
+  })
+
+  it('disables input on disabled property', async () => {
+    ptInput.setProps({ disabled: true })
+    await Vue.nextTick()
+    expect(input.attributes('disabled')).toBe('disabled')
   })
 })
--- puncto-front/tests/unit/components/PtLogo.spec.js
+++ puncto-front/tests/unit/components/PtLogo.spec.js
@@ -1,6 +1,6 @@
 import { shallowMount } from '@vue/test-utils'
-import PtIcon from '@/components/PtIcon.vue'
-import PtLogo from '@/components/PtLogo.vue'
+import PtIcon from '@/common/PtIcon.vue'
+import PtLogo from '@/common/PtLogo.vue'
 
 describe('PtLogo', () => {
   let ptLogo
@@ -14,15 +14,15 @@ describe('PtLogo', () => {
   })
 
   it('renders the logo element', () => {
-    expect(ptLogo.find('.logo')).toBeDefined()
+    expect(ptLogo.find('.logo').exists()).toBeDefined()
   })
 
   it('renders the watch icon', () => {
-    expect(ptLogo.find('.watch')).toBeDefined()
+    expect(ptLogo.find('.watch').exists()).toBeDefined()
   })
 
   it('renders the h1 tag', () => {
-    expect(ptLogo.find('h1')).toBeDefined()
+    expect(ptLogo.find('h1').exists()).toBeDefined()
   })
 
   it('renders the title "puncto" inside h1 tag', () => {
