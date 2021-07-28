76692e3897abeb02dd45ded98f6b576a24709aad
philipeatela <philipe@videopeel.com>
2021-07-28 16:24:24 -0300

    feat: added more unit tests for auth service

--- puncto-back/jest.config.js
+++ puncto-back/jest.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  collectCoverage: true,
-  collectCoverageFrom: [
-    "./out/**/*",
-    "!**/node_modules/**",
-    "!**/vendor/**"
-  ],
+  // collectCoverage: true,
+  // collectCoverageFrom: [
+  //   "./out/**/*",
+  //   "!**/node_modules/**",
+  //   "!**/vendor/**"
+  // ],
   testMatch: [
     '<rootDir>/src/**/*.unit.test.ts',
   ],
