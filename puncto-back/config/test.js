3be0867fcba2d794bd9996fddb33db8288c401c0
Philipe Pinheiro Atela <philipeatela@gmail.com>
2021-08-23 18:36:05 -0300

    feat: deploy backend to gcloud app engine (#42)
    Co-authored-by: philipeatela <philipe@videopeel.com>

--- puncto-back/config/test.js
+++ puncto-back/config/test.js
@@ -1,6 +1,7 @@
 module.exports = {
   port: '3000',
   secret: '0917B13A9091915D54B6336F45909539CCE452B3661B21F38641',
+  mongoSsl: false,
   mongoDbPort: 27018,
   mongoDbName: 'default',
   mongoDbHost: 'localhost',
