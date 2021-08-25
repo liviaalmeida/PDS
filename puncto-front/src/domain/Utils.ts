b089fe4f7f6515be1cfedec204995b03c46eb842
Lívia Almeida <leave.ah@gmail.com>
2021-08-25 14:11:37 -0300

    Add currency formater to utils

--- puncto-front/src/domain/Utils.ts
+++ puncto-front/src/domain/Utils.ts
@@ -1,4 +1,11 @@
 export class Utils {
+  static currency(num: number, currency: string): string {
+    return Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency,
+    }).format(num)
+  }
+
   static normalize(str: string): string {
     return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase()
   }
